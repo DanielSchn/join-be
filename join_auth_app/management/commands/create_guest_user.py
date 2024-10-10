0c78cfe8d82a0a646a19a8eef832f69a1858ee8a
DanielSchn <d.schneider@dsc-mail.de>
2024-10-10 16:07:45 +0200

    doc

--- join_auth_app/management/commands/create_guest_user.py
+++ join_auth_app/management/commands/create_guest_user.py
@@ -3,9 +3,27 @@ from django.contrib.auth.models import User
 from join_auth_app.models import UserProfile
 
 class Command(BaseCommand):
+    """
+    Dieses Management Command erstellt einen Gastbenutzer mit einem zugehörigen UserProfile.
+    
+    Es überprüft zuerst, ob ein Benutzer mit dem Benutzernamen 'guest' bereits existiert. 
+    Falls nicht, wird der Benutzer mit voreingestellten Werten (Benutzername, E-Mail und Passwort) 
+    sowie einem zugehörigen Profil (Initialen und Farbe) angelegt.
+    """
     help = 'Erstellt einen Gastnutzer mit einem zugehörigen UserProfile.'
 
     def handle(self, *args, **kwargs):
+        """
+        Führt den Command aus, um den Gastbenutzer und das UserProfile zu erstellen.
+        
+        Überprüft, ob der Benutzer mit dem Benutzernamen 'guest' existiert.
+        Falls nicht, wird der Benutzer erstellt, und ein zugehöriges UserProfile angelegt.
+        Gibt nach erfolgreicher Erstellung eine Bestätigungsmeldung aus oder eine Warnmeldung,
+        wenn der Benutzer bereits existiert.
+
+        Returns:
+            Gibt eine Statusmeldung über die Konsole aus.
+        """
         if not User.objects.filter(username='guest').exists():
             account = User.objects.create_user(
                 username='guest',
